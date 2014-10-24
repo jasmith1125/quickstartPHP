0c97113e06b3929ab1ae691afcfc487bea509ccb
jasmith1125 <jasmith1125@yahoo.com>
2014-10-23 21:31:59 -0400

    control structure exercise

--- contol structures--if and switch/handle_reg.php
+++ contol structures--if and switch/handle_reg.php
@@ -17,9 +17,6 @@
 // booleans are case insensitive in php
 $okay = TRUE;
 
-if ($okay) {
-	print '<p>You have been successfully registered (but not really).</p>';
-}
 
 //validate email address
 if(empty($_POST['email'])) {
@@ -33,13 +30,31 @@ if(empty($_POST['password'])) {
 	$okay = FALSE;
 }
 
+//check that the two passwords match
+if (trim($_POST['password']) != trim($_POST['confirm'])) {
+	print '<p class="error">Your confirmed password does not match the original password.</p>';
+	$okay = FALSE;
+} 
+
 // validate birth year
 if(is_numeric($_POST['year'])) {
 	$age = 2011 - $_POST['year']; //calculate age this year
 	} else {
-		print 'p class-"error">Please enter the year you were born as four digits.</p>';
+		print '<p class-"error">Please enter the year you were born as four digits.</p>';
 		$okay = FALSE;
-	}	
+}
+
+//report an error if the year is greater than or equal to 2011
+if($_POST['year'] >= 2011) {
+	print '<p class="error">Either you entered your birth year wrong or you come from the future.</p>';
+	$okay = FALSE;
+}
+
+//if there are no errors, print success message and current age
+if($okay) {
+	print '<p>You have been successfully registered (but not really).</p>';
+	print "<p>You will turn $age this year.</p>";
+}
 ?>
 </body>
 </html>
